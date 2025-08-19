43ac0f444132a59f05dc205b388c2b1fc5c2172d
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-08-19 20:35:23 +0530

    add php-cs-fixer.dist.php

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,22 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+    ->name('*.php');
+
+return (new Config())
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'no_trailing_whitespace_in_string' => true,
+        'single_quote' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha']
+    ])
+    ->setFinder($finder)
+    ->setUsingCache(true);
